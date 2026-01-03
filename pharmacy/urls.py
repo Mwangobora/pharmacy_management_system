a7d5f41d8645dae837e29c416aba7a0df5c73839
mwangobora <mwangobora37@gmail.com>
2026-01-03 20:37:48 +0300

    media file

--- pharmacy/urls.py
+++ pharmacy/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
@@ -32,4 +34,7 @@ router.register(r'purchase-items', PurchaseItemViewSet, basename='purchase-item'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-]
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
